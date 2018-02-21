79373ba331bf2dc1c3dcb12f9e1ada9e014f7ec2
papb <papb1996@gmail.com>
2018-02-21 11:49:30 -0300

    Start using jsonify-error

--- browser.js
+++ browser.js
@@ -1,33 +1,7 @@
-(function() {
-
-// Same code for node and browser:
-function err(identationLevel, msg) {
-    msg = msg.replace(/^/g, "  ".repeat(identationLevel));
-    msg = msg.replace(/(\r?\n|\r)\s*\[2+\]/g, "\n");
-    msg = msg.replace(/\r?\n|\r/g, "\n"+"  ".repeat(identationLevel));
-    return msg;
-}
-function handler(error, log) {
-    var superclasses = [];
-    var temp = Object.getPrototypeOf(Object.getPrototypeOf(error));
-    while (temp !== null) {
-        superclasses.push(temp.constructor.name);
-        temp = Object.getPrototypeOf(temp);
-    }
-    var msgs = [];
-    msgs.push(err(0, "Unhandled Error in Promise:"));
-    msgs.push(err(1, "Name: " + error.name));
-    msgs.push(err(1, "Message: " + error.message));
-    msgs.push(err(1, "Superclasses: " + superclasses.join(", ")));
-    msgs.push(err(1, "Extra fields:"));
-    for (var x in error) {
-        msgs.push(err(2, `"${x}": ` + JSON.stringify(error[x], null, 2)));
-    }
-    msgs.push(err(1, "Call stack: " + error.stack.replace(/(\r?\n|\r)    /g, "\n  ")));
-    log(msgs.join("\n"));
-}
-
-// In browser:
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+const jsonifyError = require("jsonify-error");
+window.jsonifyError = jsonifyError;
+console.log('uai');
 window.addEventListener("unhandledrejection", function(ev) {
     var reason = undefined;
     
@@ -40,8 +14,57 @@ window.addEventListener("unhandledrejection", function(ev) {
     if (!reason) {
         console.error("better-promise-error-log error: unable to find error cause. Please open an issue on github: https://github.com/papb/better-promise-error-log/issues");
     } else {
-        handler(reason, msg => console.error(msg));
+        var wrapped = jsonifyError(reason);
+        var message = "Unhandled Error in Promise: " + JSON.stringify(wrapped, null, 2);
+        console.error(message);
     }
 });
+},{"jsonify-error":2}],2:[function(require,module,exports){
+"use strict";
 
-})();
+function jsonifyError(error) {
+    var superclasses = [];
+    var temp = Object.getPrototypeOf(Object.getPrototypeOf(error));
+    while (temp !== null) {
+        superclasses.push(temp.constructor.name);
+        temp = Object.getPrototypeOf(temp);
+    }
+    var wrappedError = {};
+    wrappedError.name = error.name;
+    wrappedError.message = error.message;
+    wrappedError.superclasses = superclasses;
+    wrappedError.enumerableFields = {};
+    for (let x in error) {
+        wrappedError.enumerableFields[x] = error[x];
+    }
+    wrappedError.stack = error.stack.split('\n').map(x => x.replace(/^\s+/, ""));
+    return wrappedError;
+}
+
+function mapArgs(args) {
+    return args.map(arg => {
+        if (arg instanceof Error) return module.exports(arg);
+        return arg;
+    });
+}
+
+var alreadyOverridden = false;
+jsonifyError.overrideConsole = function() {
+    if (alreadyOverridden) return;
+    alreadyOverridden = true;
+    var defaultConsoleLog = console.log;
+    var defaultConsoleWarn = console.warn;
+    var defaultConsoleError = console.error;
+    console.log = function(...args) {
+        defaultConsoleLog.apply(null, mapArgs(args));
+    };
+    console.warn = function(...args) {
+        defaultConsoleWarn.apply(null, mapArgs(args));
+    };
+    console.error = function(...args) {
+        defaultConsoleError.apply(null, mapArgs(args));
+    };
+};
+
+module.exports = jsonifyError;
+},{}]},{},[1]);
---
+++ make_browser.js
@@ -0,0 +1,19 @@
+const jsonifyError = require("jsonify-error");
+window.jsonifyError = jsonifyError;
+window.addEventListener("unhandledrejection", function(ev) {
+    var reason = undefined;
+    
+    // Native promises puts the error in ev.reason
+    if (ev.reason) reason = ev.reason;
+
+    // Bluebird puts the error in ev.detail.reason
+    if (ev.detail && ev.detail.reason) reason = ev.detail.reason;
+
+    if (!reason) {
+        console.error("better-promise-error-log error: unable to find error cause. Please open an issue on github: https://github.com/papb/better-promise-error-log/issues");
+    } else {
+        var wrapped = jsonifyError(reason);
+        var message = "Unhandled Error in Promise: " + JSON.stringify(wrapped, null, 2);
+        console.error(message);
+    }
+});
